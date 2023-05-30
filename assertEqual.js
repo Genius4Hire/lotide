640a5bad1186b8018c85a092069711bf4bcb16e6
Jordan Dennis <jordan.dennis@gmail.com>
2023-05-30 17:08:12 +0000

    Initial commit.

---
+++ assertEqual.js
@@ -0,0 +1,9 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🤌: ${actual} === ${expected}`);
+  } else {
+    console.log(`😒 : ${actual} !== ${expected}`);
+  }
+};
+
+module.exports = assertEqual;
