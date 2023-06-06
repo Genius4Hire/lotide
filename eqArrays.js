7fe85e5a0dd19cc5695ccffbc5a0c07531a5bca2
Jordan Dennis <jordan.dennis@gmail.com>
2023-06-06 00:54:12 +0000

    w2d1 - Lotide refactor 'tail'

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🤌: ${actual} === ${expected}`);
-  } else {
-    console.log(`😒 : ${actual} !== ${expected}`);
-  }
-};
 
-// A function that compares two arrays, and returns a boolean of they match.  
-// Note:  This function assumes both arrays are sorted in the same way!
 const eqArrays = function(arr1, arr2){
   // First, if the lengths are unequal, we are done, not equivilent
   if (arr1.length !== arr2.length){
@@ -20,24 +11,10 @@ const eqArrays = function(arr1, arr2){
     if (arr1[i] !== arr2[i]) {
       return false;
     }
+    
   }
   // if we are here, the arrays are identical, so return true.
   return true;
 }
 
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-
- // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-assertEqual(eqArrays(["10", "02", "30"], ["10", "02", "30"]), true);
-
-//module.exports = eqArrays;
+module.exports = eqArrays;
