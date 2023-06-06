063801df48ee8fd1f8159cc8514b6136bd4ab6f1
Jordan Dennis <jordan.dennis@gmail.com>
2023-06-06 17:33:50 +0000

    w2d1 - Converted testing over to use Mocha & Chai for 'middleTest.js'

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,31 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
 
-// Testing..
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+describe("#middle", () => {
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.sameOrderedMembers(middle([1, 2, 3]), [2]);
+  });
+
+  it("returns [1, 2] for [0, 1, 2, 3]", () => {
+    assert.sameOrderedMembers(middle([0, 1, 2, 3]), [1, 2]);
+  });
+
+  it("returns [] for ['5']", () => {
+    assert.sameOrderedMembers(middle(['5']), []); 
+  });
+
+  it("returns [] for ['1']", () => {
+    assert.sameOrderedMembers(middle(['1']), []); 
+  });
+
+  it("returns [] for [1, 2]", () => {
+    assert.sameOrderedMembers(middle(['1, 2']), []); 
+  });
+
+  it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.sameOrderedMembers(middle([1, 2, 3, 4, 5, 6]), [3, 4]); 
+  });
+});
+
+
+
