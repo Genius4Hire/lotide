145fb708c06bb13a96276fd8f0fa82820d4d9659
Jordan Dennis <jordan.dennis@gmail.com>
2023-06-06 04:01:38 +0000

    w2d1 - Converted testing over to use Mocha & Chai for 'headTest.js'

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,19 @@
+const assert = require('chai').assert;
 const head = require('../head');
-const assertEqual = require('../assertEqual');
 
-let testArray = [1, 2, 3];
-assertEqual(head(testArray), 1);
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
 
-testArray = [0, 1, 2, 3];
-assertEqual(head(testArray), 0);
+  it("returns 0 for [0, 1, 2, 3]", () => {
+    assert.strictEqual(head([0, 1, 2, 3]), 0);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+
+});
+
